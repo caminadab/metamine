c36e2fcd35ffcf2050dbfe35306686669f46f902
ymte <ymte@pc>
2019-04-25 17:24:35 +0200

    bestandje openen in assembly

--- asm/hoi.s
+++ asm/hoi.s
@@ -5,15 +5,35 @@
 .section .text
 
 _start:
+	# Print groet
 	mov rax, 1
 	mov rdi, 1
-	lea rsi, .var[rip]
+	lea rsi, .groet[rip]
 	mov rdx, 4
 	syscall
+
+	# Open bestand
+	mov rax, 2
+	lea rdi, .bestand[rip]
+	mov rsi, 0x40
+	mov rdx, 0644
+	syscall
+
+	# Sluit bestand
+	mov rdi, rax
+	mov rax, 3
+	syscall
+
+	# Exit
 	mov rax, 60
 	mov rdi, 0
 	syscall
 	ret
 
-.var:
+.section .rodata
+
+.bestand:
+	.string "test.txt"
+
+.groet:
 	.string	"hoi\n"
