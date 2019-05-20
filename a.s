ab70278e74efac9bc8800b45c758aac788896b98
ymte <ymte@laptop>
2019-05-21 00:27:06 +0200

    grapig

--- a.s
+++ a.s
@@ -5,28 +5,22 @@
 .section .text
 
 _start:
-movq -50[rsp], 34
-movq rax, 0x6420696f68
-mov -42[rsp], rax
-movq rax, 0x67206e6164
-mov -38[rsp], rax
-movq rax, 0x2074616167
-mov -34[rsp], rax
-movq rax, 0x2074656820
+movq -38[rsp], 22
+movq rax, 0x6f6c6c6168
 mov -30[rsp], rax
-movq rax, 0x68636f7420
+movq rax, 0x726577206f
 mov -26[rsp], rax
-movq rax, 0x656f672068
+movq rax, 0x6a646c6572
 mov -22[rsp], rax
-movq rax, 0x6577206465
+movq rax, 0x617620656a
 mov -18[rsp], rax
-movq rax, 0x646c657265
+movq rax, 0x656d206e61
 mov -14[rsp], rax
-movq rax, 0x0a64
+movq rax, 0x0a65
 mov -10[rsp], rax
-lea rax, -42[rsp]
+lea rax, -30[rsp]
 mov rdi, 1
-lea rsi, -42[rsp]
+lea rsi, -30[rsp]
 movq rdx, -8[rsi]
 mov rax, 1
 syscall
